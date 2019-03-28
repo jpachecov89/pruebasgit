635dfd9b65dd9e785a2f83efc061c0fa2a823a77
Joel Pacheco V <jpachecov89@gmail.com>
2019-03-28 14:03:14 -0500

    Actualizando la Hoja de Scripts

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ function multiplicar(a,b){
 function dividir(a,b){
 	return a / b;
 }
+
+function potencia(a,b){
+	let pot = 1;
+	let i = 0;
+	for(i=0; i < b; i++){
+		pot = pot * a;
+	}
+	return pot;
+}
+
+function factoriza(a){
+	let fac = 1;
+	let i = 0;
+	for(i=1; i<=a; i++){
+		fac = fac * i;
+	}
+	return fac;
+}
